784e5daa7fe34585c7fe50f240b9a92de297e4f9
Leyhara <vocalold@naver.com>
2022-07-26 21:30:06 +0900

    Initial commit

---
+++ 1002.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Beakjoon_1002
+{
+	static void Main()
+	{
+		// 실행 해야하는 갯수
+		int i_case = int.Parse(Console.ReadLine());
+		for (int i = 0; i < i_case; i++)
+		{
+			// 데이터 입력
+			string[] s_data = Console.ReadLine().Split();
+
+			// 조규현의 데이터
+			int x1 = int.Parse(s_data[0]);
+			int y1 = int.Parse(s_data[1]);
+			int r1 = int.Parse(s_data[2]);
+
+			// 백승환의 데이터
+			int x2 = int.Parse(s_data[3]);
+			int y2 = int.Parse(s_data[4]);
+			int r2 = int.Parse(s_data[5]);
+
+			// 중심거리 계산
+			double f_distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+			// 두 원이 만나지 않을 경우
+			// r2 + r1 < d or  r2 - r1 > d
+			if (f_distance > r1 + r2 || f_distance < Math.Abs(r1 - r2) && r1 != r2)
+				Console.WriteLine(0);
+
+			// 두 원이 한 점에서 만날경우
+			// r2 - r1 = d
+			else if (f_distance == r1 + r2 || f_distance == Math.Abs(r1 - r2) && r1 != r2)
+				Console.WriteLine(1);
+
+			// 두 원이 두 점에서 만날경우
+			// r2 - r1 < d < r2 + r1
+			else if (f_distance < r1 + r2 && f_distance > Math.Abs(r1 - r2))
+				Console.WriteLine(2);
+
+			// 두 원이 겹칠경우
+			// r2 = r1 and x1 = x2 and y1 = y2
+			else
+				Console.WriteLine(-1);
+		}
+	}
+}
---
+++ 1003.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Beakjoon_1003
+{
+	static void Main()
+	{
+		// 실행 해야하는 갯수
+		int i_case = int.Parse(Console.ReadLine());
+
+		for (int i = 0; i < i_case; i++)
+		{
+			// 데이터 입력
+			int i_data = int.Parse(Console.ReadLine());
+
+			int i_result = 1;
+			int i_prev = 1;
+
+			// fibonacci(0)
+			if (i_data == 0)
+				Console.WriteLine("1 0");
+
+			// fibonacci(1)
+			else if (i_data == 1)
+				Console.WriteLine("0 1");
+
+			// fibonacci(n)
+			else
+			{
+				// 갯수 구하기
+				for (int j = 2; j < i_data; j++)
+				{                   
+					// 이전의 값을 더해서 표시
+					i_result += i_prev;
+
+					// 이전 값을 저장
+					i_prev = i_result - i_prev;
+				}
+				Console.WriteLine(i_prev + " " + i_result);
+			}
+		}
+	}
+}

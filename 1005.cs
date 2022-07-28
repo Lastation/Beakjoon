64d60aad5fdb7c188e8f65ff82e8591013911e1b
Leyhara <vocalold@naver.com>
2022-07-28 23:12:10 +0900

    Beakjoon1005

---
+++ 1005.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Beakjoon_1005
+{
+    static void Main()
+    {
+        // 실행 해야하는 갯수
+        int i_case = int.Parse(Console.ReadLine());
+
+        while (i_case > 0)
+        {
+
+        }
+    }
+}

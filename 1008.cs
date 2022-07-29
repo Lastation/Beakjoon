56ff271bb25cdda98ecd20cff1a361e804bd4c19
Leyhara <vocalold@naver.com>
2022-07-29 18:41:23 +0900

    1008

--- 1008.cs
+++ 1008.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-class Beakjoon_1001
+class Beakjoon_1008
 {
     static void Main()
     {
         // 실행 해야하는 갯수
         string[] i_data = Console.ReadLine().Split();
 
-        // A - B 를 출력
-        Console.WriteLine(int.Parse(i_data[0]) - int.Parse(i_data[1]));
+        // A / B 를 출력
+        Console.WriteLine(double.Parse(i_data[0]) / double.Parse(i_data[1]));
     }
 }
